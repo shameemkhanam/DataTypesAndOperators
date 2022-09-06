bf5157581fb7c2807af300401cb9671e9e5fa7dc
unknown <shameemkhanam@gmail.com>
2022-09-06 14:01:01 +0530

    first commit

---
+++ DataTypes.js
@@ -0,0 +1,23 @@
+var x = "hello";
+
+x = 'hello';
+
+x = 23;
+x = -34.22;
+
+x = true;
+x = false;
+
+x = undefined;
+
+x=["html","css","js"];
+
+x={
+  name : "shama",
+  state : "karnataka"
+};
+
+console.table(x);
+console.log(typeof(x));
+
+
---
+++ operators.js
@@ -0,0 +1,85 @@
+//ARITHMETIC OPERATORS
+
+// var a = 100;
+// var b = 200;
+// var res = a + b;
+
+// var a = 200;
+// var b = 100;
+// var res = a - b;
+
+// var a = 20;
+// var b = 10;
+// var res = a * b;
+
+// var a = 200;
+// var b = 100;
+// var res = a / b;
+
+// var a = 5;
+// var b = 2;
+// var res = a % b;
+
+// var a = 2;
+// var b = 3;
+// var res = a ** b;
+
+// var a = 2;
+// var b = 2;
+// a++;
+// var res = a + b;
+// console.log(res);
+
+// var z = 40;
+// z--;
+// console.log(z);
+
+//----------------------------------------------------
+
+//ASSIGNMENT OPERATORS
+
+/*var x =20;
+var y=2;
+x +=y; //x=x+y
+x-=y;
+x*=y;
+x**=y;
+x/=y;
+x%=y;
+
+console.log(x);  */
+
+//---------------------------------------------
+
+//COMPARISON OPERATORS
+
+/*var a = 10;
+var b = "10";
+// console.log(a==b);
+// console.log(a===b); //checks with data type also
+
+// console.log(2 === "2");
+
+// console.log(2!=2);
+// console.log(2!=="2");
+// console.log((4>20));
+// console.log(4<20);
+// console.log(20<=20);
+console.log(23>=9);*/
+
+//------------------------------------------------
+//LOGICAL OPERATORS
+
+var age = 22;
+
+// if(age>=18 && age<=21){
+//   console.log("u r eligible");
+// }
+
+// if(age>=18 || age<=21){
+//   console.log("u r eligible");
+// }
+
+if(!(age<22)){
+  console.log("eligible");
+}
