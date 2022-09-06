a0eb5db3f6bd2f71ddbaa15fdf26e8edaabd58a1
unknown <shameemkhanam@gmail.com>
2022-09-06 23:42:13 +0530

    2nd commit

---
+++ ArrayMethods.js
@@ -0,0 +1,70 @@
+/*let arr = [10,20,30,40];
+// console.log(arr);
+let sum=0;
+for(let i=0; i<arr.length; i++){
+  console.log(arr[i]);
+  sum +=arr[i];
+}
+console.log(`The sum of array elements = ${sum}`); */
+
+// let arr2 = [34,"harry",true,null];
+// console.log(arr2);
+
+
+/*let arr3 = new Array(23,34,55);
+
+// let arr3 = new Array();
+// arr3[0]=67;
+// arr3[1]="harry";
+// arr3[2]="shama";
+// arr3[3]=44;
+console.table(arr3); */
+//----------------------------
+// let arr4 = [
+//   [10,20,30],
+//   [40,50,60]  
+// ];
+// console.log(arr4);
+//console.table(arr4);
+
+//----------------------------------- 
+// let arr5 = [10,30,50];
+// arr5[1] = 20;
+// delete arr5[2];
+// // console.table(arr5);
+// console.log(arr5);
+
+//=======================================
+//ARRAY METHODS
+//========================================
+
+var a = ["sallu","abbu","neetu","meetu"];
+// console.log(a.sort());
+//----------------------------------------------
+// var a = [20,43,12,34];
+// // a.sort((a,b)=> a-b); //asc order
+// a.sort((a,b)=> b-a); // desc order
+// console.log(a);
+//----------------------------------------------
+// a.reverse();
+// console.log(a);
+//---------------------------------------------
+
+// a.pop();
+// a.push("zeetu");
+// a.shift();
+// a.unshift("newValue");
+// console.log(a);
+//----------------------------------------------
+
+let ar1 = ["a","b","c"] ;
+let ar2 = ["d","e"];
+let ar3 = ["f","g"];
+// console.log(ar1.concat(ar2,ar3));
+
+// console.log(ar1.join(" "));
+console.log(ar1.join("/"));
+
+
+
+
---
+++ StringMethods.js
@@ -0,0 +1,59 @@
+// var str = " Javascript is a Scripting Language";
+// console.log(str.length);
+
+// console.log(str.toLowerCase());
+
+// console.log(str.toUpperCase());
+
+// console.log(str.includes("scr"));
+// console.log(str.includes("SCRIPTING"));
+
+// console.log(str.startsWith("Ja"));
+
+// console.log(str.endsWith("age"));
+
+// console.log(str.search("is"));
+
+// console.log(str.match(/r/g));
+
+// console.log(str.indexOf("is"));
+
+// console.log(str.lastIndexOf("is"));
+
+// console.log(str.replace("Javascript","PHP"));
+// console.log(str.replace(/Javascript/g,"PHP"));
+// // console.log(str.replaceAll("Javascript","PHP"));
+
+// console.log(str.trim());
+
+// console.log(str.charAt(10));
+// // console.log(str.charAt(100));
+
+// console.log(str.charCodeAt(2));
+
+/*var str2 = " hello";
+var str3 = " world";
+console.log(str.concat(str2,str3));*/
+
+var str4 = "Hello World ";
+// console.log(str4.split("e"));
+
+// console.log(str4.repeat(4));
+
+// console.log(str4.slice(1,7));
+// console.log(str4.slice(2));
+// console.log(str4.slice(-5));
+
+// console.log(str4.substr(2));
+// console.log(str4.substr(2,5));
+
+// console.log(str4.substring(2,7));
+
+var a = 45;
+var b = a.toString()
+console.log(typeof(a));
+console.log(typeof(b));
+
+console.log(b.valueOf());
+
+
