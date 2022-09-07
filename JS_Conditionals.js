9cee8f810d17c7c23bc0d9e1f7652bc3bbc4156c
unknown <shameemkhanam@gmail.com>
2022-09-07 23:39:42 +0530

    07_09_2022 commit

---
+++ JS_Conditionals.js
@@ -0,0 +1,59 @@
+// IF- ELSE
+
+// var fname = "seema";
+// var gender = "male";
+
+// if(gender == "female"){
+//   console.log(`hi, Miss ${fname}`);
+// }
+// else{
+//   console.log(`hi, Mr. ${fname}`);
+// }
+//==========================================
+//IF-ELSE-IF
+
+// var per = 67;
+// if(per>=80 && per<=100){
+//   console.log("u r in merit");
+// }
+// else if(per>=60 && per<80){
+//   console.log("1st division");
+// }
+// else if(per>=50 && per<60){
+//   console.log("2nd division");
+// }
+// else if(per < 35){
+//   console.log("fail");
+// }
+// else{
+//   console.log("plz enter valid percentage");
+// }
+
+//=======================================================
+//TERNARY
+
+// var a=210;
+// var result = (a==20) ?  "matches" : "no match";
+// console.log(result);
+
+//=======================================================
+//SWITCH
+
+var day = 2;
+switch (day) {
+  case 0:
+    console.log("Monday");
+    break;
+
+  case 1:
+    console.log("Tuesday");
+    break;
+
+  case 2:
+    console.log("Wednesday");
+    break;
+
+  default:
+    console.log("enter values 0 - 2");
+    break;
+}
